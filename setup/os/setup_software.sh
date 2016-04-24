6036e29b575ee453b71d787b3cc48c213986ea07
Thomas Lomas <tlomas@gmail.com>
2016-04-24 19:25:14 +0100

    Add some more used software

--- setup/os/setup_software.sh
+++ setup/os/setup_software.sh
@@ -26,7 +26,14 @@ install_package "Git" "git"
 install_package "GNOME Vim" "vim-gnome"
 install_package "tmux" "tmux"
 install_package "VirtualBox" "virtualbox"
+install_package "Caffeine" "caffeine"
 
+print_in_green "\n  ---\n\n"
+sudo add-apt-repository ppa:kilian/f.lux
+update
+install_package "Flux" "fluxgui"
+
+print_in_green "\n  ---\n\n"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
